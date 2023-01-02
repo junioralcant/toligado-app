69500eb82a937bd9a0f7eb8bdf4d4944171bad33
junioralcant <junioralcant111@gmail.com>
2023-01-02 17:57:49 -0300

    feat: add authentication usecase

---
+++ src/domain/usecases/authentication.ts
@@ -0,0 +1,20 @@
+export interface IAuthentication {
+  auth(params: IAuthentication.Params): Promise<IAuthentication.Model>;
+}
+
+export namespace IAuthentication {
+  export type Params = {
+    cpf: string;
+  };
+
+  export type Model = {
+    user: {
+      belongsCompany: string;
+      blockedUser: boolean;
+      id: string;
+      name: string;
+      cpf: string;
+    };
+    token: string;
+  };
+}
