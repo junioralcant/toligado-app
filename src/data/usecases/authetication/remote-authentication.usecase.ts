f5b1cbb44c4d827e9b2979e586142df63326830d
junioralcant <junioralcant111@gmail.com>
2023-01-02 18:44:13 -0300

    feat: ensure RemoteAuthenticationUseCase calls HttpPostClient with conrrect URL

---
+++ src/data/usecases/authetication/remote-authentication.usecase.ts
@@ -0,0 +1,12 @@
+import { IHttpPostClient } from '../../repositories/http/http-post-client';
+
+export class RemoteAuthenticationUseCase {
+  constructor(
+    private readonly url: string,
+    private readonly httpPostClientSpy: IHttpPostClient
+  ) {}
+
+  async auth(): Promise<void> {
+    this.httpPostClientSpy.post(this.url);
+  }
+}
