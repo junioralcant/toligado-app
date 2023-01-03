2b554b8c64d2154eef29b9fbbee6b731fc777603
junioralcant <junioralcant111@gmail.com>
2023-01-03 15:40:45 -0300

    feat: ensure AxiosHttpClient call axios.pos with correct url

---
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -0,0 +1,16 @@
+import {
+  HttpPostParams,
+  HttpResponse,
+  IHttpPostClient,
+} from '@data/repositories/http';
+import axios from 'axios';
+
+export class AxiosHttpClient implements IHttpPostClient {
+  async post(params: HttpPostParams): Promise<HttpResponse<any>> {
+    await axios.post(params.url);
+    return {
+      statusCode: 200,
+      body: {},
+    };
+  }
+}
