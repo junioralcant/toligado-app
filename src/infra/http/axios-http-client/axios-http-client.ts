f103d9ffaf80b8a1a70e536e26d935cbdfcbfc98
junioralcant <junioralcant111@gmail.com>
2023-01-03 17:05:55 -0300

    feat: ensure AxiosHttpClient axios.post return correct values

--- src/infra/http/axios-http-client/axios-http-client.ts
+++ src/infra/http/axios-http-client/axios-http-client.ts
@@ -3,14 +3,21 @@ import {
   HttpResponse,
   IHttpPostClient,
 } from '@data/repositories/http';
-import axios from 'axios';
+import axios, { AxiosResponse } from 'axios';
 
 export class AxiosHttpClient implements IHttpPostClient {
-  async post(params: HttpPostParams): Promise<HttpResponse<any>> {
-    await axios.post(params.url, params.body);
+  async post(params: HttpPostParams): Promise<HttpResponse> {
+    let axiosResponse: AxiosResponse;
+
+    axiosResponse = await axios.post(params.url, params.body);
+
+    return this.adapt(axiosResponse);
+  }
+
+  private adapt(axiosResponse: AxiosResponse): HttpResponse {
     return {
-      statusCode: 200,
-      body: {},
+      statusCode: axiosResponse.status,
+      body: axiosResponse.data,
     };
   }
 }
