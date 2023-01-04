e3ad29fcf4e04505e0059a8b1557a3d05bd9e918
junioralcant <junioralcant111@gmail.com>
2023-01-04 17:02:57 -0300

    feat: create screen Login

--- src/presentation/screens/login/index.tsx
+++ src/presentation/screens/login/index.tsx
@@ -1,9 +1,64 @@
-import { Container, Text } from './styles';
+import { useState } from 'react';
+import { KeyboardAvoidingView } from 'react-native';
+
+import girl from '@presentation/assets/images/girl.png';
+import logo from '@presentation/assets/images/logorocha.png';
+
+import {
+  BoxButton,
+  BoxGirl,
+  Button,
+  Container,
+  Form,
+  Input,
+  InputText,
+  Label,
+  Girl,
+  TextButton,
+  BoxLogo,
+  Logo,
+} from './styles';
 
 export function Login() {
+  const [isFocused, setIsFocused] = useState(false);
+
+  function handleInputFocus() {
+    setIsFocused(true);
+  }
+
+  function handleInputBlur() {
+    setIsFocused(false);
+  }
+
   return (
     <Container>
-      <Text>Screen Login</Text>
+      <BoxGirl>
+        <Girl source={girl} />
+      </BoxGirl>
+
+      <Form>
+        <KeyboardAvoidingView behavior="padding">
+          <Input>
+            <Label>CPF</Label>
+            <InputText
+              isFocused={isFocused}
+              placeholder="Infome seu CPF"
+              onFocus={handleInputFocus}
+              onBlur={handleInputBlur}
+            />
+          </Input>
+        </KeyboardAvoidingView>
+
+        <BoxButton>
+          <Button>
+            <TextButton>LOGIN</TextButton>
+          </Button>
+        </BoxButton>
+      </Form>
+
+      <BoxLogo>
+        <Logo source={logo} />
+      </BoxLogo>
     </Container>
   );
 }
--- src/presentation/screens/login/styles.ts
+++ src/presentation/screens/login/styles.ts
@@ -1,9 +1,91 @@
-import styled from 'styled-components/native';
+import { RFValue } from 'react-native-responsive-fontsize';
+import styled, { css } from 'styled-components/native';
+import { getStatusBarHeight } from '../../../../node_modules/react-native-iphone-x-helper/index';
+
+type ProsInputText = {
+  isFocused: boolean;
+};
 
 export const Container = styled.View`
-  background: ${({ theme }) => theme.colors.background_primary};
+  flex: 1;
+  width: 100%;
+  justify-content: center;
+  background: ${({ theme }) => theme.colors.background_secondary};
+  padding: 0px 24px;
 `;
 
-export const Text = styled.Text`
-  font-family: ${({ theme }) => theme.fonts.bold};
+export const Content = styled.View`
+  background: red;
+`;
+
+export const BoxGirl = styled.View`
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+`;
+
+export const Girl = styled.Image.attrs({
+  resizeMode: 'contain',
+})`
+  width: 320px;
+  height: 300px;
+`;
+
+export const Form = styled.View``;
+
+export const Input = styled.View``;
+
+export const Label = styled.Text`
+  font-size: ${RFValue(15)}px;
+  color: ${({ theme }) => theme.colors.text_label};
+  margin-bottom: 4px;
+`;
+
+export const InputText = styled.TextInput<ProsInputText>`
+  width: 100%;
+  height: 55px;
+  border-radius: 10px;
+  padding-left: 15px;
+
+  ${({ isFocused, theme }) =>
+    isFocused
+      ? css`
+          border: 1px ${theme.colors.title};
+        `
+      : css`
+          border: 1px ${theme.colors.text_label};
+        `}
+`;
+
+export const BoxButton = styled.View`
+  margin-top: 15px;
+`;
+
+export const Button = styled.TouchableOpacity`
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  background-color: ${({ theme }) => theme.colors.background_primary};
+  width: 100%;
+  height: 55px;
+  border-radius: 10px;
+`;
+
+export const TextButton = styled.Text`
+  font-size: ${RFValue(16)}px;
+  color: ${({ theme }) => theme.colors.text};
+`;
+
+export const BoxLogo = styled.View`
+  width: 100%;
+  align-items: center;
+  justify-content: center;
+  margin-top: 15px;
+`;
+
+export const Logo = styled.Image.attrs({
+  resizeMode: 'contain',
+})`
+  width: 100px;
+  height: 40px;
 `;
