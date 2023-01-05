9ba5dcef0dac418b3a139c5dbecf8eb38c6d478d
junioralcant <junioralcant111@gmail.com>
2023-01-05 09:45:26 -0300

    refactor: move SUT creation to a factory method

--- src/presentation/screens/login/login.spec.tsx
+++ src/presentation/screens/login/login.spec.tsx
@@ -3,14 +3,17 @@ import { render, screen } from '@testing-library/react-native';
 import { ThemeProvider } from 'styled-components/native';
 import { Login } from '.';
 
+function makeSut(): void {
+  render(
+    <ThemeProvider theme={theme}>
+      <Login />
+    </ThemeProvider>
+  );
+}
+
 describe('Login Screen', () => {
   it('Shoul start with inital state disabled', () => {
-    render(
-      <ThemeProvider theme={theme}>
-        <Login />
-      </ThemeProvider>
-    );
-
+    makeSut();
     const inputCPF = screen.getByPlaceholderText('Informe seu CPF');
     const button = screen.getByTestId('LOGIN');
     expect(inputCPF.props.isFocused).toBeFalsy();
