98208452fa21738696b863e08d78520a0479749e
junioralcant <junioralcant111@gmail.com>
2023-01-06 16:25:23 -0300

    refactor: move SUT creation to a factory method

--- src/validation/validation/cpf/cpf-validation.spec.ts
+++ src/validation/validation/cpf/cpf-validation.spec.ts
@@ -1,15 +1,19 @@
 import { InvalidCpfError } from '@validation/errors';
 import { CpfValidation } from './cpf-validation';
 
+function makeSut(field: string): CpfValidation {
+  return new CpfValidation(field);
+}
+
 describe('CpfValidation', () => {
   it('Should return error if CPF is invalid', () => {
-    const sut = new CpfValidation('cpf');
+    const sut = makeSut('cpf');
     const error = sut.validate('04403030360');
     expect(error).toEqual(new InvalidCpfError());
   });
 
   it('Should not return error if CPF is valid', () => {
-    const sut = new CpfValidation('cpf');
+    const sut = makeSut('cpf');
     const error = sut.validate('157.861.480-50');
     expect(error).toBeFalsy();
   });
