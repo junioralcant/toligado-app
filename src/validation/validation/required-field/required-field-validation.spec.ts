0e2ba2688474fdcdaac6dfc271b00789f38ccd60
junioralcant <junioralcant111@gmail.com>
2023-01-06 15:35:44 -0300

    test: ensure RequiredFieldValidation not retorn error if filed is not empty

--- src/validation/validation/required-field/required-field-validation.spec.ts
+++ src/validation/validation/required-field/required-field-validation.spec.ts
@@ -14,4 +14,11 @@ describe('RequiredFieldValidation', () => {
     const error = sut.validate('');
     expect(error).toEqual(new RequiredFieldError());
   });
+
+  it('Should not return error if field is not empty', () => {
+    const field = faker.database.column();
+    const sut = new RequiredFieldValidation(field);
+    const error = sut.validate(faker.random.words());
+    expect(error).toBeFalsy();
+  });
 });
