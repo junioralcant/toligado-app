fd8ad291d1cc7cff163ecf3edb744f8f0e0213e4
junioralcant <junioralcant111@gmail.com>
2023-01-06 16:22:39 -0300

    test: ensure CpfValidation not return error if CPF is valid

--- src/validation/validation/cpf/cpf-validation.spec.ts
+++ src/validation/validation/cpf/cpf-validation.spec.ts
@@ -2,9 +2,15 @@ import { InvalidCpfError } from '@validation/errors';
 import { CpfValidation } from './cpf-validation';
 
 describe('CpfValidation', () => {
-  it('Should rentur error if CPF is invalid', () => {
+  it('Should return error if CPF is invalid', () => {
     const sut = new CpfValidation('cpf');
     const error = sut.validate('04403030360');
     expect(error).toEqual(new InvalidCpfError());
+  });
+
+  it('Should not return error if CPF is valid', () => {
+    const sut = new CpfValidation('cpf');
+    const error = sut.validate('157.861.480-50');
+    expect(error).toBeFalsy();
   });
 });
