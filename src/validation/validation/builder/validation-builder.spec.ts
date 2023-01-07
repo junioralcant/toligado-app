f5f455c1949690ffc7e52ca64079bac4fb36e5ec
junioralcant <junioralcant111@gmail.com>
2023-01-07 16:54:51 -0300

    refactor: add faker values in ValidationBuilder

--- src/validation/validation/builder/validation-builder.spec.ts
+++ src/validation/validation/builder/validation-builder.spec.ts
@@ -1,21 +1,22 @@
+import { faker } from '@faker-js/faker';
 import { RequiredFieldValidation, CpfValidation } from '@validation/validation';
 import { ValidationBuilder } from './validation-builder';
 
 describe('ValidationBuilder', () => {
   it('Shoul return RequiredFieldValidation', () => {
-    const fieldName = 'any_field';
+    const fieldName = faker.database.column();
     const validatoins = ValidationBuilder.field(fieldName).required().build();
     expect(validatoins).toEqual([new RequiredFieldValidation(fieldName)]);
   });
 
   it('Shoul return CpfValidation', () => {
-    const fieldName = 'any_field';
+    const fieldName = faker.database.column();
     const validatoins = ValidationBuilder.field(fieldName).cpf().build();
     expect(validatoins).toEqual([new CpfValidation(fieldName)]);
   });
 
   it('Shoul return a list of validations', () => {
-    const fieldName = 'any_field';
+    const fieldName = faker.database.column();
     const validatoins = ValidationBuilder.field(fieldName)
       .required()
       .cpf()
