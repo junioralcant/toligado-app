0738f282c967c8a6ae87133a40e350aeda3bb40e
junioralcant <junioralcant111@gmail.com>
2023-01-09 17:21:07 -0300

    test: ensure AsyncStorageAdapter.setTem is call with correct values

---
+++ src/infra/async-storage/async-storage.spec.ts
@@ -0,0 +1,25 @@
+import AsyncStorage from '@react-native-async-storage/async-storage';
+import { mockAccounModel } from '@domain/mocks';
+import { AsyncStorageAdapter } from './asyn-storage';
+
+jest.mock('@react-native-async-storage/async-storage', () =>
+  require('@react-native-async-storage/async-storage/jest/async-storage-mock')
+);
+
+function makeSut(): AsyncStorageAdapter {
+  return new AsyncStorageAdapter();
+}
+
+describe('AsyncStorageAdapter', () => {
+  it('Should call asyncStorage.setTem with correct values', () => {
+    const sut = makeSut();
+    const key = 'any_key';
+    const value = mockAccounModel();
+
+    sut.set(key, value);
+    expect(AsyncStorage.setItem).toHaveBeenCalledWith(
+      key,
+      JSON.stringify(value)
+    );
+  });
+});
