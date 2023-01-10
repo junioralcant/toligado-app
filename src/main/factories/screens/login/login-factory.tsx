dc4127dd5393bea980b625e73b06b9f2e7978c0a
junioralcant <junioralcant111@gmail.com>
2023-01-10 19:37:17 -0300

    feat: change Login composition to add AsynStorageAdapter

--- src/main/factories/screens/login/login-factory.tsx
+++ src/main/factories/screens/login/login-factory.tsx
@@ -1,4 +1,5 @@
 import { RemoteAuthenticationUseCase } from '@data/usecases/authentication/remote-authentication.usecase';
+import { AsyncStorageAdapter } from '@infra/async-storage/asyn-storage-adapter';
 import { AxiosHttpClient } from '@infra/http/axios-http-client/axios-http-client';
 import { makeApiUrlFactory } from '@main/factories/services/api-url-factory';
 import { Login } from '@presentation/screens/Login';
@@ -12,11 +13,13 @@ export function MakeLoginFactory() {
     axiosHttpClient
   );
   const validationComposite = makeLoginValidationFactory();
+  const asyncStorage = new AsyncStorageAdapter();
 
   return (
     <Login
       authentication={remoteAuthentication}
       validation={validationComposite}
+      asyncStorage={asyncStorage}
     />
   );
 }
