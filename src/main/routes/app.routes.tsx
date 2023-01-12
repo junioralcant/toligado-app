7e272367da630e76e3b251c440de9a8ae2a1e1a1
junioralcant <junioralcant111@gmail.com>
2023-01-12 15:55:18 -0300

    feat: create screen Home

--- src/main/routes/app.routes.tsx
+++ src/main/routes/app.routes.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { MakeLoginFactory } from '@main/factories/screens/login/login-factory';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
+import { Home } from '@presentation/screens/Home';
 
 const { Navigator, Screen } = createNativeStackNavigator();
 
@@ -8,6 +9,7 @@ export function AppRoutes() {
   return (
     <Navigator screenOptions={{ headerShown: false }}>
       <Screen name="login" component={MakeLoginFactory} />
+      <Screen name="home" component={Home} />
     </Navigator>
   );
 }
