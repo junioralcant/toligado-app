e38fd5e02d850c9c9659dc616e3847853f160d0d
junioralcant <junioralcant111@gmail.com>
2023-03-19 12:09:23 -0300

    feat: enseure that SelectImage open camera

--- src/infra/select-image/select-image.ts
+++ src/infra/select-image/select-image.ts
@@ -3,8 +3,16 @@ import * as ImagePicker from 'expo-image-picker';
 import { ISelectImage } from '@data/repositories/select-image';
 
 export class SelectImage implements ISelectImage {
-  openCamera(): Promise<object> {
-    throw new Error('Method not implemented.');
+  async openCamera(): Promise<object> {
+    const result = await ImagePicker.launchCameraAsync({
+      mediaTypes: ImagePicker.MediaTypeOptions.All,
+      aspect: [4, 3],
+      quality: 1,
+    });
+
+    const dataImage = result.assets;
+
+    return dataImage ? dataImage[0] : {};
   }
 
   async openGallery(): Promise<object> {
