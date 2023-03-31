fda9c33b9fabb7d9184b1f911e5be4c20e2d0199
junioralcant <junioralcant111@gmail.com>
2023-03-31 17:28:48 -0300

    test: ensure that RemoteCreateRecordUseCase return UnexpectedErro if status erro 403

--- src/data/usecases/create-record/remote-create-record.usecase.spec.ts
+++ src/data/usecases/create-record/remote-create-record.usecase.spec.ts
@@ -1,6 +1,9 @@
 import { faker } from '@faker-js/faker';
 import { HttpPostClientSpy } from '@data/mocks';
 import { RemoteCreateRecordUseCase } from './remote-create-record.usecase';
+import { HttpStatusCode } from '@data/repositories/http';
+import { mockCreateRecordParams } from '@domain/mocks';
+import { UnexpectedError } from '@domain/errors';
 
 type SutType = {
   sut: RemoteCreateRecordUseCase;
@@ -20,7 +23,19 @@ describe('RemoteCreateRecordUseCase', () => {
   it('Should call HttpClient with correct URL', async () => {
     const url = faker.internet.url();
     const { sut, httpPostClientSpy } = makeSut(url);
-    await sut.create();
+    await sut.create(mockCreateRecordParams());
     expect(httpPostClientSpy.url).toBe(url);
+  });
+
+  it('Should throw error if HttpPostClient returns 403 ', async () => {
+    const url = faker.internet.url();
+    const { sut, httpPostClientSpy } = makeSut(url);
+    httpPostClientSpy.response = {
+      statusCode: HttpStatusCode.forbidden,
+    };
+
+    const promise = sut.create(mockCreateRecordParams());
+
+    await expect(promise).rejects.toThrow(new UnexpectedError());
   });
 });
