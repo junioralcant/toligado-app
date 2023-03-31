4e445ba6c848c19b053e02361e98910b8c30878a
junioralcant <junioralcant111@gmail.com>
2023-03-31 10:38:15 -0300

    test: ensure that SelectImage open camera

---
+++ src/infra/select-image/select-image.spec.ts
@@ -0,0 +1,44 @@
+import { SelectImage } from './select-image';
+import * as ImagePicker from 'expo-image-picker';
+
+type SutTypes = {
+  sut: SelectImage;
+};
+
+function mockeSelectImage(): ImagePicker.ImagePickerAsset[] {
+  return [
+    {
+      assetId: null,
+      base64: null,
+      duration: null,
+      exif: null,
+      height: 1280,
+      type: 'image',
+      uri: 'file:///data/user/0/host.exp.exponent/cache/ExperienceData/%2540jrmarques.dev%252Ftoligado-app/ImagePicker/5fa8404d-87ec-44a7-8d86-378321c73310.jpeg',
+      width: 960,
+    },
+  ];
+}
+
+function makeSut(): SutTypes {
+  const sut = new SelectImage();
+
+  return {
+    sut,
+  };
+}
+
+describe('SelectImage', () => {
+  it('Should select camera image', async () => {
+    const { sut } = makeSut();
+    const value = mockeSelectImage();
+
+    jest.spyOn(ImagePicker, 'launchCameraAsync').mockResolvedValueOnce({
+      assets: value,
+    });
+
+    const result = await sut.openCamera();
+
+    expect(result).toEqual(value[0]);
+  });
+});
